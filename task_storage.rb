c99ff40fc4f918dd2ba71bc10e2f394cd668e2f1
Antigrapist <antigrapist@gmail.com>
2016-12-15 17:18:44 -0500

    creates method to add input to the db.

--- task_storage.rb
+++ task_storage.rb
@@ -3,15 +3,28 @@
 require "sqlite3"
 
 
-db = db.execute <<-SQL
-	create table tasks (
-	name varchar(255),
-	email varchar(255),
-	task varchar(255),
-	do_on date
+db = SQLite3::Database.new("tasks.db")
 
-	);
+
+create_table = <<-SQL
+	create table IF NOT EXISTS tasks(
+		name varchar(255),
+		email varchar(255),
+		task varchar(255),
+		task_catagory varchar(255),
+		do_on date
+	)
 SQL
 
 
+
+def add_task_to_db(db, name, email, task, task_catagory, do_on)
+	db.execute(
+		"Insert into tasks (name, email, task, task_catagory, do_on) VALUES (?, ?, ?, ?, ?)", [name, email, task, task_catagory, do_on] )
+end
+
+
+
+
+db.execute(create_table) 
 
