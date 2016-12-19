d3ce784a73a15d35920294ec4b946e0a40ce14b2
Antigrapist <antigrapist@gmail.com>
2016-12-18 22:03:35 -0500

    fleshed out the basics for the storage module

--- task_storage.rb
+++ task_storage.rb
@@ -18,13 +18,37 @@ SQL
 
 
 
-def add_task_to_db(db, name, email, task, task_catagory, do_on)
+def add_task_to_db(db, user_name, email, task, task_details, do_on)
 	db.execute(
-		"Insert into tasks (name, email, task, task_catagory, do_on) VALUES (?, ?, ?, ?, ?)", [name, email, task, task_catagory, do_on] )
+		"Insert into tasks (name, email, task, task_details, do_on) VALUES (?, ?, ?, ?, ?)", [name, email, task, task_details, do_on] )
 end
 
 
 
+task_user_name = "Alex"
+email_address = "Alex@AlexRaubach.com"
+
+# ui
 
 db.execute(create_table) 
 
+puts "How many tasks would you like to input?"
+loop_variable = gets.chomp.to_i
+
+
+loop_variable.times do |i|
+
+	puts "What do you want to be reminded about?"
+	user_provided_task = gets.chomp
+
+	puts "What details would you like to remind yourself about later?"
+	user_provided_details = gets.chomp
+
+	puts "When do you want to be reminded about #{user_provided_task}?"
+	user_provided_date = gets.chomp
+
+
+	add_task_to_db(db, task_user_name, email_address, user_provided_task, user_provided_details, user_provided_date)
+end
+
+puts "Have a good day"
