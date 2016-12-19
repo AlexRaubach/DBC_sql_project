f87027d73639eebde501de4a0bee68881b1b0c06
Antigrapist <antigrapist@gmail.com>
2016-12-19 01:00:03 -0500

    added details about gmail functionality

--- task_retrieval.rb
+++ task_retrieval.rb
@@ -1,4 +1,20 @@
 
+require_relative "gmail.rb" #this contains my gmail user name and password, so I'm not uploading it to uploaded to github. 
+
+# Here's the contents of that file minus my gmail credentials: 
+
+	# require 'mail'
+	# options = { :address              => "smtp.gmail.com",
+	#             :port                 => 587,
+	#             :domain               => 'alexraubach.com',
+	#             :user_name            => '<username>',
+	#             :password             => '<password>',
+	#             :authentication       => 'plain',
+	#             :enable_starttls_auto => true  }
+	            
+	# Mail.defaults do
+	#   delivery_method :smtp, options
+	# end
 
 
 
